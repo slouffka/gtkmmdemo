fdb01dbe879db9fa530d45cdb91105a4ef4eadbc
Vyacheslav Moskalev <slouffka@gmail.com>
2021-03-14 02:44:52 +0700

    Fix main.cpp formatting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "./ExampleApplication.h"
 
 int main(int argc, char* argv[]) {
-    auto app = ExampleApplication::create();
-    return app->run(argc, argv);
+	auto app = ExampleApplication::create();
+	return app->run(argc, argv);
 }
